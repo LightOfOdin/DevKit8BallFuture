23e0667c7c5bd9e2820e6216a8a970f93aa324df
David Oraha <davidoraha2206@outlook.com>
2019-10-16 18:12:13 +1100

    Added detailed comments
    Added coments to code that goes indepth, towards how sensors are initalised, how their pointers are set and where and how they're being referenced.

--- Device/8BallFuture.ino
+++ Device/8BallFuture.ino
@@ -6,12 +6,13 @@
 long randNumber;
 
 //init sensors
-DevI2C *i2c;
-LSM6DSLSensor *sensor;
+DevI2C *i2c; // a helper class that provides functions for ic2 Communication the various sensors.
+LSM6DSLSensor *sensor; //This is the abstract class of the IMU, selects the various sensors relating to gyro and Accelerometer.
 
 //init step variables for changing from idle state to hasShake (Telling Future).
 int steps = 0;
-static int state = 0; // this is an idle state
+static int state = 0; // state = 0 is an idle state (enterIdleState).
+                      // state = 1 is returned to change state to hasShake and will read out the future.
 
 //setting empty array to contain possible futures in string format.
 static char *futures[] = {
@@ -37,7 +38,6 @@ static char *futures[] = {
   "Very doubtful."
 };
 
-
 //This is the main screen to prompt the user to shake for their future
 static void enterIdleState() {
   state = 0;
@@ -59,7 +59,16 @@ bool hasShake() {
   }
 }
 
-//Initialising sensor variables, enabling fn' and setting sensitivity of Pedometre for the hasShake function
+/** 
+ * A serial connection is Initialied with a 9600 hz clock speed, for the gyroscope and Accelerometer sensor.
+ * i2c variable holds the pinModes (Physical pin connections for the Accelerometer and Gyroscope).
+ * These are then parsed into a sensor variable that points-to the pins serial connection.
+ * As the sensor variable is holding the gyroscope funcion. The Pedameter function is enabled referencing
+ * enablePedometer based on the gyroscope construct.
+ * Within the pedometer, the sensor's registrable threshold is set to register for a mid to low velosity/shake.
+ * check https://microsoft.github.io/azure-iot-developer-kit/docs/apis/lsm6dsl/ for more infomation.
+ **/
+
 void setup() {
   Serial.begin(9600);
   i2c = new DevI2C(D14, D15);
@@ -73,13 +82,17 @@ void setup() {
   enterIdleState();
 }
 
-//Loop that runs main fn. If shake is true pick ranom number, then resetSteps to look for new shake.
+/**The main code loop runs here. 
+ * If hasShake is true, picks a random number of 20, the 20 indexes of the futures[] array.
+ * Before outputted, runs through the randNumber variable to allow randomSeed generation.
+**/
+
 void loop() {
   if (hasShake()) {
     Screen.clean();
     randNumber = random(20);
     Screen.print(1, futures[randNumber], true);
-    delay(5000);
+    delay(5000); // displays future for 5 seconds
     sensor->resetStepCounter();
   } 
   if (state == 0) {
